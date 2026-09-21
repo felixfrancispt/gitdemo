f35f1f8febf3a64e17dc7f2797410fb386331362
Felix <felixfrancis.pt@gmail.com>
2026-09-21 12:44:35 -0700

    Added student input feature

--- student.py
+++ student.py
@@ -1,2 +1,8 @@
 print("Student management System")
 print("Welcome to Git Lab")
+
+name = input("enter student name:")
+course =input("Enter course: ")
+
+print("Student Name:", name)
+print("Course:", course)
